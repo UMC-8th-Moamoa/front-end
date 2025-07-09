ff5f15c615843b9e4bc671defb6c7278c40d4717
JINI0617 <seojin8476@naver.com>
2025-07-09 23:21:28 +0900

    Feat: add page indicator

--- src/components/HomePage/Banner/SubBannerCarousel.tsx
+++ src/components/HomePage/Banner/SubBannerCarousel.tsx
@@ -16,7 +16,7 @@ const SubBannerCarousel = () => {
   const currentItem = dummySubBanners[currentIndex];
 
   return (
-    <div className="w-[393px] flex justify-center py-2 space-y-2 transition-all duration-500">
+    <div className="w-[393px] flex flex-col items-center py-2 space-y-2 transition-all duration-500">
       <SubBanner
         imageSrc={currentItem.imageSrc}
         content={currentItem.content}
@@ -24,6 +24,18 @@ const SubBannerCarousel = () => {
         onClick={currentItem.onClick}
         variant={currentItem.variant}
       />
+
+      {/* 페이지네이션 인디케이터 */}
+      <div className="flex justify-center space-x-[6px] mt-2">
+        {dummySubBanners.map((_, index) => (
+          <div
+            key={index}
+            className={`w-2 h-2 rounded-full transition-all duration-300 ${
+              index === currentIndex ? 'bg-gray-700' : 'bg-gray-300'
+            }`}
+          />
+        ))}
+      </div>
     </div>
   );
 };
