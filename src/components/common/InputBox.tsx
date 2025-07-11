1af6c442a1b8a6687ac4542424972a931dca254c
JINI0617 <seojin8476@naver.com>
2025-07-11 15:56:55 +0900

    Fix: modify InputBox raius

--- src/components/common/InputBox.tsx
+++ src/components/common/InputBox.tsx
@@ -1,7 +1,7 @@
 interface InputBoxProps extends React.InputHTMLAttributes<HTMLInputElement> {
   type?: "text" | "password" | "email" | "number" | "search";
   className?: string;
-  hasBorder?: boolean;
+  hasBorder?: boolean; 
 }
 
 const InputBox = ({
@@ -13,7 +13,7 @@ const InputBox = ({
   return (
     <input
       type={type}
-      className={`w-[350px] h-[50px] px-4 py-2 rounded-md text-sm placeholder-gray-400
+      className={`w-[350px] h-[50px] px-4 py-2 rounded-[12px] text-sm placeholder-gray-400
         ${hasBorder
           ? "border border-red-500"
           : "appearance-none outline-none border-none"} 
