eb060949a7240636f4a074441c588008cd413ca3
dojmj <dojmj@naver.com>
2025-07-29 15:47:42 +0900

    fix: Add error props in Inputbox

--- src/components/common/InputBox.tsx
+++ src/components/common/InputBox.tsx
@@ -4,23 +4,32 @@ interface InputBoxProps extends React.InputHTMLAttributes<HTMLInputElement> {
   type?: "text" | "password" | "email" | "number" | "search";
   className?: string;
   hasBorder?: boolean;
+  error?: string; //오류 메시지 추가
 }
 
 const InputBox = ({
   type = "text",
   className = "",
   hasBorder = true,
+  error,
   ...props
 }: InputBoxProps) => {
   return (
-    <input
-      type={type}
-      className={`w-[350px] h-[50px] px-4 py-2 rounded-[12px] text-sm 
-        text-[#1F1F1F] placeholder:text-[#97B1FF] bg-[#E7EDFF]
-        ${hasBorder ? "border border-[#97B1FF]" : "appearance-none outline-none border-none"} 
-        ${className}`}
-      {...props}
-    />
+    <div className="relative w-[350px]">
+      <input
+        type={type}
+        className={`w-full h-[50px] px-4 py-2 rounded-[12px] text-sm 
+          text-[#1F1F1F] placeholder:text-[#97B1FF] bg-[#E7EDFF]
+          ${hasBorder ? "border border-[#97B1FF]" : "appearance-none outline-none border-none"} 
+          ${className}`}
+        {...props}
+      />
+      {error && (
+        <p className="absolute top-full mt-1 text-xs text-[#E20938]">
+          {error}
+        </p>
+      )}
+    </div>
   );
 };
 
