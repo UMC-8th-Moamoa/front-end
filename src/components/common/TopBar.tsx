d127420a8f45130e32a092d09b156f6bd2b97581
JINI0617 <seojin8476@naver.com>
2025-08-05 13:35:58 +0900

    Fix: 충돌해결

--- src/components/common/TopBar.tsx
+++ src/components/common/TopBar.tsx
@@ -1,30 +1,23 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import AlarmIcon from "../../assets/Alarm.svg";
 import SearchIcon from "../../assets/Search.svg";
 import MoamoaLogo from "../../assets/MoamoaLogo.svg";
 
 const TopBar = () => {
+  const navigate = useNavigate();
+
   return (
-<<<<<<< HEAD
-    <div className="w-[350px] h-[44px] px-[20px] flex items-center justify-between mx-auto fixed top-0 left-1/2 -translate-x-1/2 z-[1000] bg-[#FFF]">
-      {/* 알람 아이콘 */}
-      <div className="w-[24px] h-[24px] ml-[10px] flex items-center justify-center">
-        <img
-          src={AlarmIcon}
-          alt="알람"
-          className="w-full h-full object-contain"
-=======
-    <div className="w-[393px] h-14 relative flex items-center justify-center px-4 ">
+    <div className="w-[393px] h-14 relative flex items-center justify-center px-4 bg-[#FFF] fixed top-0 left-1/2 -translate-x-1/2 z-[1000]">
       {/* 좌측 알람 아이콘 */}
       <div
-        onClick={() => navigate("/alarm")} 
+        onClick={() => navigate("/alarm")}
         className="absolute left-8 cursor-pointer"
       >
         <img
-          src="/assets/Alarm.svg"
-          alt="알림"
-          className="w-[20px] h-[20px] mt-[2px] select-none"
->>>>>>> origin/develop
+          src={AlarmIcon}
+          alt="알람"
+          className="w-[24px] h-[24px] object-contain"
         />
       </div>
 
@@ -37,24 +30,15 @@ const TopBar = () => {
         />
       </div>
 
-<<<<<<< HEAD
-      {/* 검색 아이콘 */}
-      <div className="w-[24px] h-[24px] flex items-center mr-[10px] justify-center">
-        <img
-          src={SearchIcon}
-          alt="검색"
-          className="w-full h-full object-contain"
-=======
-      {/* 우측 돋보기 아이콘 */}
+      {/* 우측 검색 아이콘 */}
       <div
         onClick={() => navigate("/search")}
         className="absolute right-8 cursor-pointer"
       >
         <img
-          src="/assets/Search.svg"
+          src={SearchIcon}
           alt="검색"
-          className="w-[21px] h-[21px] select-none"
->>>>>>> origin/develop
+          className="w-[21px] h-[21px] object-contain"
         />
       </div>
     </div>
