8065684465d00bb329f5517eaf4c55441daa84ad
Hyejun Koo <khjcoco3387@ewhain.net>
2025-08-06 16:41:19 +0900

    fix: camera 경로수정

--- src/pages/MoaLetter/SelectPhotoPage.tsx
+++ src/pages/MoaLetter/SelectPhotoPage.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import BackButton from "../../components/common/BackButton";
-import CameraIcon from "../../assets/camera.svg";
+import CameraIcon from "../../assets/Camera.svg";
 
 interface AlbumItemProps {
   title: string;
