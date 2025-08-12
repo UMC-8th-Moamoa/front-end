e0c738455165ac8c1791809844612cd0d1b927df
dojmj <dojmj@naver.com>
2025-08-12 16:40:32 +0900

    feat: signup-login merge

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -4,9 +4,9 @@ import { Link, useLocation, useNavigate } from "react-router-dom";
 import InputBox from "../../components/common/InputBox";
 import Button from "../../components/common/Button";
 import { Modal } from "../../components/common/Modal";
-import KakaoIcon from "../assets/Kakao.svg";
-import Logo from "../assets/Logo_white.svg";
-import moa from "../assets/moa_character.svg";
+import KakaoIcon from "../../assets/Kakao.svg";
+import Logo from "../../assets/Logo_white.svg";
+import moa from "../../assets/moa_character.svg";
 import api from "../../api/axiosInstance"; 
 
 type SuccessEnvelope<T> = {
