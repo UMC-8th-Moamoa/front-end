249701460cb15bbcf7b56f2d5b18f629c641ab4e
혜준 <167407164+HyejunKoo@users.noreply.github.com>
2025-08-20 17:57:12 +0900

    Delete src directory

--- src/components/common/Spinner.tsx
+++
@@ -1,9 +0,0 @@
-function Spinner() {
-  return (
-    <div className="flex justify-center items-center w-full h-full">
-      <div className="animate-spin rounded-full h-10 w-10 border-4 border-gray-300 border-t-transparent" />
-    </div>
-  );
-}
-
-export default Spinner;
